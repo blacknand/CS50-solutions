c7ad34a6ce0aab0c01e7319b663a68735a254fd1
Nathan Blackburn <nblackburndeveloper@icloud.com>
2023-10-16 17:35:10 +0100

    Week 6 solutions

---
+++ week6/pset6mario.py
@@ -0,0 +1,19 @@
+# TODO
+
+while True:
+    try:
+        size = int(input("Size: "))
+        if size <= 8 and size >= 1:
+            break
+    except ValueError:
+        print("Not an int")
+
+n = len(range(size))
+
+for i in range(size):
+    for j in range(size):
+        if i + j < n - 1:
+            print(" ", end='')
+        else:
+            print("#", end='')
+    print()
